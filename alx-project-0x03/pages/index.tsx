785f154edc63c9b7d961876eec035b0dea040bcb
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2025-11-22 07:28:13 +0100

    add tailwind v3 styles

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,17 +1,9 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import Layout from "@/components/layouts/Layout";
+const Home = () => {
+  return (
+    <div>
+      <h1> Welcome to Splash App </h1>
+    </div>
+  );
+};
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export default function Home() {
-  return <Layout>This is the main layour</Layout>;
-}
+export default Home;
