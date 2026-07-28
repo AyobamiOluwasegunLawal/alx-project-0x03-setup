af8ec5576cbba51b6f3252dabf768b6adc872f86
Ayobami <ayobamioluwasegslawal@gmail.com>
2026-07-28 08:58:21 +0100

    create directory for interface

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
-
-interface LayoutProps {
-    children: React.ReactNode
-}
+import { LayoutProps } from '@/interface'
 
 const Layout = ({children}: LayoutProps) => {
   return (
