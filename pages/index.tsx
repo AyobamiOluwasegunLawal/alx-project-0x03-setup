1c11d95e5a8204c0194ea8c59505bb437b4eb5e3
Ayobami <ayobamioluwasegslawal@gmail.com>
2026-07-28 08:27:39 +0100

    correct foolder name and add font style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,10 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import Layout from "@/components/layouts/Layout";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Layout from "@/component/layouts/Layout";
 
 export default function Home() {
   return (
     <Layout>
       <div
-      className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
+      className={`flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
     >
       This is the home page
     </div>
